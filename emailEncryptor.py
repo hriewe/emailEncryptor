d2c864fe7c4faa5fc427ad70e99d518c3f60b236
Hayden Riewe <43478556+hriewe@users.noreply.github.com>
2018-10-13 14:46:14 -0400

    Add files via upload

---
+++ emailEncryptor.py
@@ -0,0 +1,52 @@
+import yagmail
+import os
+import random
+import sys
+
+def encrypt(string):
+  shift = random.randint(1,26)
+  cipher = ''
+  for char in string: 
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+
+  cipher += ' '
+  cipher += str(shift)
+  return cipher
+
+def decodeMail():
+
+
+def sendMail():
+	usrEmail = input("Great! What is your e-mail address? ")
+	usrPass = input("What is your password? ")
+	yag = yagmail.SMTP(usrEmail, usrPass)
+	content = input("Please enter the body of the e-mail: ")
+	cryptContent = encrypt(content)
+
+	sendTo = input("Who do you want to send this to? ")
+	subject = input("Subject line? ")
+
+	yag.send(sendTo, subject, cryptContent)
+
+	print("Message sent successfully!!!")
+	again = input("Send another message? (yes/no) ")
+	if again == 'yes' or again == 'Yes':
+		sendMail()
+	else:
+		sys.exit(0)
+
+
+print("\nWelcome to emailEncryptor!\n")
+print(" Writen by Hayden Riewe  \n")
+decision = input("Would you like to send, or decode mail? ")
+
+if decision == "send":
+	sendMail()
+
+if decision == 'decode'
+	decodeMail()
